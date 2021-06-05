7e6be564f5c3de6c2256dca2cd168478fd4fc1a9
Christian-is <79840294+Christian-is@users.noreply.github.com>
2021-06-05 11:58:30 +0200

    Add files via upload

---
+++ src/main/java/it/uniba/main/Costanti/Comandi.java
@@ -0,0 +1,19 @@
+package it.uniba.main.Costanti;
+
+/**
+ *  <<noECB>>
+ *  La classe Comandi contiene le costanti dei comandi di gioco possibili
+ */
+public final class Comandi {
+    public static final String HELP = "help";
+    public static final String NUMERI = "numeri";
+    public static final String DAMIERA = "damiera";
+    public static final String TEMPO = "tempo";
+    public static final String GIOCA = "gioca";
+    public static final String MOSSE = "mosse";
+    public static final String PRESE = "prese";
+    public static final String ABBANDONA = "abbandona";
+    public static final String ESCI = "esci";
+    public static final String PATTA = "patta";
+    private Comandi() { }
+}
